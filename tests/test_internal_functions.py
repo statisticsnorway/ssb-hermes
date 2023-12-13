77a749345c42b1a140082766c87bba0a7afa5f2e
EdvardGarmannslund <132445493+EdvardGarmannslund@users.noreply.github.com>
2023-12-13 17:51:40 +0100

    er jeg ferdig?

--- tests/test_internal_functions.py
+++ tests/test_internal_functions.py
@@ -7,5 +7,5 @@ from ssb_hermes._functions import _check_for_value
 data = pd.DataFrame({"col1": ["hei"]})
 
 
-def test__check_for_value():
+def test__check_for_value() -> None:
     assert _check_for_value(data, "col1", "hei") is True
