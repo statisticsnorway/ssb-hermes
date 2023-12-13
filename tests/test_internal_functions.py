5f1ba425af659e473c0588839423c9517158cb43
EdvardGarmannslund <132445493+EdvardGarmannslund@users.noreply.github.com>
2023-12-13 17:38:18 +0100

    siste test

--- tests/test_internal_functions.py
+++ tests/test_internal_functions.py
@@ -1,17 +1,19 @@
 """Script for å test funksjoner i data kategorien for transaksjonsdata"""
 
 import os
+
 import pandas as pd
-import pytest
 
 notebook_path = os.getcwd()
 for folder_level in range(50):
-    if "pyproject.toml" in os.listdir(): break
+    if "pyproject.toml" in os.listdir():
+        break
     os.chdir("../")
 
 from src.ssb_hermes._functions import _check_for_value
 
-data = pd.DataFrame({"col1":["hei"]})
+data = pd.DataFrame({"col1": ["hei"]})
+
 
 def test__check_for_value():
-    assert _check_for_value(data,"col1","hei") == True
+    assert _check_for_value(data, "col1", "hei") == True
