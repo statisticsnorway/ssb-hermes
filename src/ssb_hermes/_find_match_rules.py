3d5783dcb970fe1c47c6dc39267f867c17e0b369
Garmannslund <ged@ssb.no>
2023-12-28 11:10:52 +0100

    alternativ funksjon for find match

--- src/ssb_hermes/_find_match_rules.py
+++ src/ssb_hermes/_find_match_rules.py
@@ -17,7 +17,7 @@ from ._functions import _find_closest_value
 from ._functions import _get_value_from_df
 
 
-def _find_match_rule1(list_with_one: list[str]) -> tuple[str, int]:
+def _find_match_one_posible(list_with_one: list[str]) -> tuple[str, int]:
     """Function for rule 1 in adress matching function. If there is only one unit at a location(postnr), then we use this unit.
 
     Args:
@@ -27,13 +27,12 @@ def _find_match_rule1(list_with_one: list[str]) -> tuple[str, int]:
         tuple: item, rule
     """
     item = list_with_one[0]
-    rule = 1
-    return item, rule
+    return item
 
 
-def _find_match_rule2(
+def _find_match_fuzzy(
     query: str, choices: list[str], score_cutoff: int = 75
-) -> tuple[Any, Any]:
+) -> Any:
     """Function for rule 2 in adress matching function. If there are multiple units we use fuzzywuzzy with 75% match.
 
     Args:
@@ -42,7 +41,7 @@ def _find_match_rule2(
         score_cutoff: The score to beat in order to be considered a match.
 
     Returns:
-        tuple: item, rule
+        tuple: item
     """
     try:
         item, match = process.extractOne(
@@ -50,59 +49,4 @@ def _find_match_rule2(
         )
     except (ValueError, AttributeError, TypeError):
         item = None
-    finally:
-        if item is None:
-            rule = None
-        else:
-            rule = 2
-    return item, rule
-
-
-def _find_match_rule3(
-    df_data_subset: pd.DataFrame,
-    df_registry_subset: pd.DataFrame,
-    postnr: str,
-    adresse: str,
-    columns: tuple[str, str, str, str, str],
-) -> tuple[Any, Any]:
-    """Function for rule 3 in adress matching function. If rule 1 and 2 did not work, we iterate up geographically.
-
-    Args:
-        df_data_subset: Subset of the data dataframe.
-        df_registry_subset: Subset of the registry dataframe.
-        postnr: The postnr to match against.
-        adresse: The adresse to match against.
-        columns: The columns to match against.
-
-    Returns:
-        tuple: item, rule
-    """
-    kommunenr = _get_value_from_df(df_data_subset, columns[2], columns[3], postnr)
-    # kommunenr = df_data_subset[df_data_subset[columns[2]] == postnr].reset_index().at[0, columns[3]]
-    fylkenr = _get_value_from_df(df_data_subset, columns[2], columns[4], postnr)
-    # fylkenr = df_data_subset[df_data_subset[columns[2]] == postnr].reset_index().at[0, columns[4]]
-
-    item = None
-    rule = None
-
-    if _check_for_value(df_registry_subset, columns[3], kommunenr):
-        df_registry_subset2 = df_registry_subset.loc[
-            df_registry_subset[columns[3]] == kommunenr
-        ]
-        item = _find_closest_value(
-            df_registry_subset2, columns[1], adresse, score_cutoff=75
-        )
-
-    if item is None:
-        if _check_for_value(df_registry_subset, columns[4], fylkenr):
-            df_registry_subset2 = df_registry_subset.loc[
-                df_registry_subset[columns[4]] == fylkenr
-            ]
-            item = _find_closest_value(
-                df_registry_subset2, columns[1], adresse, score_cutoff=75
-            )
-
-    if item is not None:
-        rule = 3
-
-    return item, rule
+    return item
--- src/ssb_hermes/functions.py
+++ src/ssb_hermes/functions.py
@@ -11,147 +11,13 @@ from typing import Any
 import pandas as pd  # type: ignore
 
 """Importing other internal functions"""
-from ._find_match_rules import _find_match_rule1
-from ._find_match_rules import _find_match_rule2
-from ._find_match_rules import _find_match_rule3
+from ._find_match_rules import _find_match_one_posible
+from ._find_match_rules import _find_match_fuzzy
 from ._functions import _add_row
 from ._functions import _check_all_values_equal
 from ._functions import _check_for_value
 from ._functions import _create_list_df_unique_value
 from ._functions import _find_postnr_through_adress
-
-
-def find_match(
-    df_data: pd.DataFrame,
-    df_registry: pd.DataFrame,
-    *columns: str,
-    registry_type_columns: str,
-    find_postnr: bool = False,
-) -> tuple[Any, Any, Any]:
-    """Fuction for matching adresses from data to registry.
-
-    Args:
-        df_data: Dataframe containing the data to be matched.
-        df_registry: Dataframe containing the registry.
-        *columns: Columns containing the data to be matched. The first column should be the group.
-        registry_type_columns: Columns containing the registry type.
-        find_postnr: Boolean value. If True, the function will try to find the postnr through the adress. Default is False.
-
-    Returns:
-        tuple: A tuple containing rows to make the matched df from, filtered df with wrong postid, filtered df with fauilty adresses.
-
-    Raises:
-        ValueError: If the number of columns is not 5.
-    """
-    if len(columns) != 5:
-        raise ValueError("Exactly five columns are required.")
-    # Teller enheter
-    i = 0
-    items = []
-    cant_find_postnr = []
-    row_failed_no_adress = []
-
-    for group in df_data[columns[0]].unique():
-        # Subsetter ut gjeldende foretak fra vof og data inn
-        df_registry_subset = df_registry.loc[df_registry[columns[0]] == group, :]
-        df_data_subset = df_data.loc[df_data[columns[0]] == group, :]
-
-        # Itererer gjennom et og et postnummer.
-        for postnr in df_data_subset[columns[2]].unique():
-            liste_data = None
-            liste_registry = None
-            postnr_registry = None
-
-            liste_data = _create_list_df_unique_value(
-                df_data_subset, columns[1], columns[2], postnr
-            )
-
-            # Sjekker om postnr finns på foretaket i vof
-            if _check_for_value(df_registry_subset, columns[2], postnr):
-                postnr_registry = postnr
-            else:
-                if find_postnr is True:
-                    item = None
-
-                    item = _find_postnr_through_adress(
-                        df_registry_subset,
-                        liste_data,
-                        postnr,
-                        columns[1],
-                        columns[2],
-                    )
-
-                    if item is not None:
-                        postnr_registry = item
-                    else:
-                        cant_find_postnr.append(
-                            df_data_subset[df_data_subset[columns[1]] == postnr]
-                        )
-                        continue
-                else:
-                    continue
-
-            liste_registry = _create_list_df_unique_value(
-                df_registry_subset, columns[1], columns[2], postnr_registry
-            )
-
-            # Itererer gjennom hver adresse vi får inn på gitt orgnr og postnummer
-            for adresse in liste_data:
-                item = None
-                # Sjekker om det kun finnes en enhet på postnummeret i vof. Isåfall bruker jeg denne enheten
-                if len(liste_registry) == 1:
-                    item, rule = _find_match_rule1(liste_registry)
-                # Om det fins flere enheter på samme postnr bruker jeg fuzzywuzzy for å matche dem.
-                else:
-                    if _check_all_values_equal(
-                        df_registry_subset, registry_type_columns
-                    ):
-                        score_cutoff = 50
-                    else:
-                        score_cutoff = 75
-                    item, rule = _find_match_rule2(
-                        adresse, liste_registry, score_cutoff=score_cutoff
-                    )
-                    # Hvis fuzzy matching ikke funker, sjekker jeg om alle enhetene har samme nace. Isåfall setter jeg bare på en enhet som ikke har None som adresse.
-
-                    # Hvis regel 2 ikke funket, bruker jeg regel 3 til å se etter nærmeste enhet.
-                    if item is None:
-                        item, rule = _find_match_rule3(
-                            df_data_subset,
-                            df_registry_subset,
-                            postnr,
-                            adresse,
-                            columns,
-                        )
-                        # Dersom jeg ikke finner noen match, legger jeg til enheten i en liste.
-                        if item is None:
-                            row_failed_no_adress.append(
-                                df_data_subset[df_data_subset[columns[1]] == adresse]
-                            )
-                            # Hvis adressen ikke finnes fortsetter vi paa neste adresse.
-                            continue
-
-                # Store information about ident from data and registry as a dict to be made into a df.
-                items.append(
-                    _add_row(
-                        columns,
-                        group,
-                        adresse,
-                        item,
-                        postnr_registry,
-                        postnr,
-                        rule,
-                    )
-                )
-
-                i += 1
-
-                if i % 1000 == 0:
-                    print(f"Vi har nådd nr {i} av {len(df_data)}")
-
-    print(f"Ferdig å kjøre {i} enhteter, fant {len(items)} enheter.")
-    return items, cant_find_postnr, row_failed_no_adress
-
 
 def get_test_data() -> tuple[pd.DataFrame, pd.DataFrame]:
     """Function for getting test data.
@@ -205,3 +71,178 @@ def get_test_data() -> tuple[pd.DataFrame, pd.DataFrame]:
         },
     )
     return df1, df2
+
+    
+
+
+def _find_match_one_posible(list_with_one: list[str]) -> tuple[str, int]:
+    """Function for rule 1 in adress matching function. If there is only one unit at a location(postnr), then we use this unit.
+
+    Args:
+        list_with_one: List with one unit at a location(postnr).
+
+    Returns:
+        tuple: item, rule
+    """
+    item = list_with_one[0]
+    rule = 1
+    return item, rule
+
+
+def _find_match_fuzzy(
+    query: str, choices: list[str], score_cutoff: int = 75
+) -> tuple[Any, Any]:
+    """Function for rule 2 in adress matching function. If there are multiple units we use fuzzywuzzy with 75% match.
+
+    Args:
+        query: The query string to match.
+        choices: List of choices to match against.
+        score_cutoff: The score to beat in order to be considered a match.
+
+    Returns:
+        tuple: item, rule
+    """
+    try:
+        item, match = process.extractOne(
+            query=query, choices=choices, score_cutoff=score_cutoff
+        )
+    except (ValueError, AttributeError, TypeError):
+        item = None
+    finally:
+        if item is None:
+            rule = None
+        else:
+            rule = 2
+    return item, rule
+
+def find_match(
+    df_data: pd.DataFrame,
+    df_registry: pd.DataFrame,
+    *columns: str,
+    registry_type_columns: str,
+    find_postnr: bool = False,
+) -> tuple[Any, Any, Any]:
+    """Fuction for matching adresses from data to registry.
+
+    Args:
+        df_data: Dataframe containing the data to be matched.
+        df_registry: Dataframe containing the registry.
+        *columns: Columns containing the data to be matched. The first column should be the group.
+        registry_type_columns: Columns containing the registry type.
+        find_postnr: Boolean value. If True, the function will try to find the postnr through the adress. Default is False.
+
+    Returns:
+        tuple: A tuple containing rows to make the matched df from, filtered df with wrong postid, filtered df with fauilty adresses.
+
+    Raises:
+        ValueError: If the number of columns is not 5.
+    """
+    if len(columns) != 5:
+        raise ValueError("Exactly five columns are required.")
+    # Counter for units
+    i = 0
+    # List for storing matched units
+    items = []
+    # List for storing units where we could not find.
+    no_match = []
+
+    for group in df_data[columns[0]].unique():
+        # Subsetting out group from data and registry
+        df_registry_subset = df_registry.loc[df_registry[columns[0]] == group, :]
+        df_data_subset = df_data.loc[df_data[columns[0]] == group, :]
+
+        # Making list for country
+        list_country = df_registry_subset[columns[1]].unique().list()
+
+        # Setting score_cutoff for fuzzywuzzy
+        if _check_all_values_equal(
+                df_registry_subset, registry_type_columns
+            ):
+            score_cutoff = 50
+        else:
+            score_cutoff = 75
+
+        # Iterating through each postnr
+        for postnr in df_data_subset[columns[2]].unique():
+            # Getting kommunenr and fylkenr from data
+            kommune = df_data_subset.loc[df_data_subset[columns[2]] == postnr, columns[4]].unique()[0]
+            fylke = df_data_subset.loc[df_data_subset[columns[2]] == postnr, columns[5]].unique()[0]
+
+            # Subsetting out postnr from data
+            df_data_subset_postnr = df_data_subset.loc[df_data_subset[columns[2]] == postnr, :]
+
+            # Making list of adresses for each geographical level.
+            list_postnr = df_registry_subset.loc[df_registry_subset[columns[2]] == postnr, columns[1]].list()
+            list_kommune = df_registry_subset.loc[df_registry_subset[columns[4]] == kommune, columns[1]].list()
+            list_fylke = df_registry_subset.loc[df_registry_subset[columns[5]] == fylke, columns[1]].list()
+
+            # Iterating through each adresse in postnr
+            for adresse in df_data_subset_postnr[columns[1]].unique():
+                item = None
+                # Checking if there is only one unit in the postnr in the registry. If so, using this unit.
+                if len(list_postnr) == 1:
+                    item, rule = _find_match_one_posible(list_postnr)
+                    rule = 1
+                # If there are more than one unit in the postnr, using fuzzywuzzy to match them.
+                # We check on each geographical level from lowest to highest.
+                if item is None:
+                    item, rule = _find_match_fuzzy(
+                        adresse, list_postnr, score_cutoff=score_cutoff
+                    )
+                    rule = 2
+                
+                if item is None:
+                    item, rule = _find_match_fuzzy(
+                        adresse, list_kommune, score_cutoff=score_cutoff
+                    )
+                    rule = 3
+                
+                if item is None:
+                    item, rule = _find_match_fuzzy(
+                        adresse, list_fylke, score_cutoff=score_cutoff
+                    )
+                    rule = 4
+                
+                if item is None:
+                    item, rule = _find_match_fuzzy(
+                        adresse, list_country, score_cutoff=score_cutoff
+                    )
+                    rule = 5
+                
+                # If fuzzy matching does not work, we check if all units have the same nace.
+                # If so, we use the best matching adress in the country.
+                if item is None:
+                    if _check_all_values_equal(
+                        df_registry_subset, registry_type_columns
+                    ):
+                        score_cutoff = 0
+                        item, rule = _find_match_fuzzy(
+                            list_country, score_cutoff=score_cutoff
+                        )
+                        rule = 6
+                
+                if item is None:
+                    no_match.append(
+                        df_data_subset_postnr[df_data_subset_postnr[columns[1]] == adresse]
+                    )
+                else:
+
+                    items.append(
+                        _add_row(
+                            columns,
+                            group,
+                            adresse,
+                            item,
+                            postnr,
+                            postnr,
+                            rule,
+                        )
+                    )
+                
+                i += 1
+
+                if i % 1000 == 0:
+                    print(f"Vi har nådd nr {i} av {len(df_data)}")
+
+    print(f"Ferdig å kjøre {i} enhteter, fant {len(items)} enheter.")
+    return items, no_match     
